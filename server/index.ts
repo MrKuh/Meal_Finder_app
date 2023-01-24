e7eb53d5e08164ff803c00b907c6b3f5cee67967
Michael Wajand <wajmia18@htl-kaindorf.at>
2023-01-24 13:42:20 +0100

    first commit

---
+++ server/index.ts
@@ -0,0 +1,32 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Meal Finder Server');
+});
+/*
+app.get('/categories', (req: Request, res: Response) => {
+    getCategories().then(data => {
+        console.log(data);
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.json(data);
+    });
+})
+
+async function getCategories(): Promise<any> {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    const data = await response.json();
+    return data;
+}
+*/
+
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
